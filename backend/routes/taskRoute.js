99e8b33cf408d42dd48064cff3615bdb84ecacc8
Your Name <kumarprince08081999@gmail.com>
2024-05-24 00:26:57 +0530

    newcommit

--- backend/routes/taskRoute.js
+++ backend/routes/taskRoute.js
@@ -4,10 +4,10 @@ const router = express.Router();
 const { createTask, getTasks, getTask, deleteTask, updateTask } = require("../controllers/taskController");
 
 
-router.post("/", createTask);    // Create a Task
-router.get("/", getTasks);      // Get/Read Tasks
-router.get("/:id", getTask);      // Get/Read single Task
-router.delete("/:id", deleteTask);      // Get/Read single Task
-router.put("/:id", updateTask);      // Get/Read single Task
+router.post("/", createTask);            // Create a Task
+router.get("/", getTasks);              // Get/Read Tasks
+router.get("/:id", getTask);            // Get/Read single Task
+router.delete("/:id", deleteTask);      // Delete a Task
+router.put("/:id", updateTask);        // Update a Task
 
 module.exports = router;
